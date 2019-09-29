0bba0b9413e56c9ebb4a546a9e17a4aca38c3a12
kemikal <janne@kemikal.org>
2019-09-29 10:31:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class Hello360 extends React.Component {
+  render() {
+    return (
+      <span>
+        <View style={styles.panel}>
+          <View style={styles.greetingBox}>
+            <Text style={styles.greeting}>
+              HiveAndFive 
+            </Text>
+            <Text style={styles.greeting}>
+              A web in 360 
+            </Text>
+          
+          </View>
+        </View>
+        <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            HiveAndFive 
+          </Text>
+          <Text style={styles.greeting}>
+            A web in 360 
+          </Text>
+          
+        </View>
+      </View>
+     </span>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('Hello360', () => Hello360);
